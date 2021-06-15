555a6948047d67c1aa4352a49fe78f082d3449b6
Katerina Paltoglou <katpaltoglou@gmail.com>
2021-06-15 09:51:00 +0300

    update branch with transformed code

--- src/node.js
+++ src/node.js
@@ -1,12 +1,4 @@
-/**
-* A simple Node that represents a single tile on the grid.
-* @param {Object} parent The parent node.
-* @param {Number} x The x position on the grid.
-* @param {Number} y The y position on the grid.
-* @param {Number} costSoFar How far this node is in moves*cost from the start.
-* @param {Number} simpleDistanceToTarget Manhatten distance to the end point.
-**/
-module.exports = function(parent, x, y, costSoFar, simpleDistanceToTarget) {
+var mod_anonymus = function(parent, x, y, costSoFar, simpleDistanceToTarget) {
     this.parent = parent;
     this.x = x;
     this.y = y;
@@ -20,3 +12,36 @@ module.exports = function(parent, x, y, costSoFar, simpleDistanceToTarget) {
         return this.costSoFar + this.simpleDistanceToTarget;
     }
 };
+
+/**
+* A simple Node that represents a single tile on the grid.
+* @param {Object} parent The parent node.
+* @param {Number} x The x position on the grid.
+* @param {Number} y The y position on the grid.
+* @param {Number} costSoFar How far this node is in moves*cost from the start.
+* @param {Number} simpleDistanceToTarget Manhatten distance to the end point.
+**/
+mod_anonymus = function(parent, x, y, costSoFar, simpleDistanceToTarget) {
+    this.parent = parent;
+    this.x = x;
+    this.y = y;
+    this.costSoFar = costSoFar;
+    this.simpleDistanceToTarget = simpleDistanceToTarget;
+
+    /**
+    * @return {Number} Best guess distance of a cost using this node.
+    **/
+    this.bestGuessDistance = function() {
+        return this.costSoFar + this.simpleDistanceToTarget;
+    }
+};
+
+/**
+* A simple Node that represents a single tile on the grid.
+* @param {Object} parent The parent node.
+* @param {Number} x The x position on the grid.
+* @param {Number} y The y position on the grid.
+* @param {Number} costSoFar How far this node is in moves*cost from the start.
+* @param {Number} simpleDistanceToTarget Manhatten distance to the end point.
+**/
+export { mod_anonymus as node };
